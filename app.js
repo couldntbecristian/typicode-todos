e1daa8f43f698d510e64eee1acc508b8f142dc4b
Cristian Cabrera <cristiancabrera@Cristians-iMac-3.home>
2023-04-08 18:49:20 -0700

    show dark background to completed todos

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos?_limit=5'
 const todoList = document.getElementById('todo-list')
-const addBtn = document.getElementById('AddBtn')
+const addBtn = document.getElementById('addBtn')
 const todoInput = document.getElementById('title')
 
 function fetchTodo(){
   fetch(apiUrl)
     .then(res => res.json())
-    .then(data => data.forEach(todo => displayTodo(todo)))
+    .then(data => {
+      console.log(data)
+      data.forEach(todo => displayTodo(todo))
+    })
 }
 
 function displayTodo(data){
   const todoDiv = document.createElement('div')
   const todoTitle = document.createTextNode(data.title) 
 
-  todoDiv.appendChild(todoTitle)
+  if(data.completed){
+    todoDiv.classList = 'done'
+  }
 
+  todoDiv.appendChild(todoTitle)
   todoList.appendChild(todoDiv)
 }
 
